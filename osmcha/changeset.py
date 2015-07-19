9f4757b8c3002e963453c54b5983730a9fe43226
Wille Marcel <wille@wille.blog.br>
2015-07-19 12:17:32 -0300

    add docstrings to all functions and classes

--- osmcha/changeset.py
+++ osmcha/changeset.py
@@ -20,6 +20,7 @@ def changeset_info(changeset):
 
 
 def get_bounds(changeset):
+    """Get the bounds of the changeset and return it as a MultiPoint object."""
     return MultiPoint([
         (float(changeset.get('min_lon')), float(changeset.get('min_lat'))),
         (float(changeset.get('max_lon')), float(changeset.get('max_lat')))
@@ -27,22 +28,28 @@ def get_bounds(changeset):
 
 
 def get_changeset(changeset):
+    """Get the changeset using OSM API and return the content as a XML
+    ElementTree.
+    """
     url = 'http://www.openstreetmap.org/api/0.6/changeset/%s/download' % changeset
     return ET.fromstring(requests.get(url).content)
 
 
 def get_metadata(changeset):
+    """Get the metadata of the changeset using OSM API and return it as a XML
+    ElementTree.
+    """
     url = 'http://www.openstreetmap.org/api/0.6/changeset/%s' % changeset
     return ET.fromstring(requests.get(url).content).getchildren()[0]
 
 
 class ChangesetList(object):
-    """Read replication changeset and return a list with information of all
-    changesets.
+    """Read replication changeset file  and return a list with information about
+    all changesets.
     """
 
-    def __init__(self, url, geojson=None):
-        self.read_file(url)
+    def __init__(self, changeset_file, geojson=None):
+        self.read_file(changeset_file)
         if geojson:
             self.get_area(geojson)
             self.filter()
@@ -50,34 +57,44 @@ class ChangesetList(object):
             self.content = self.xml.getchildren()
         self.changesets = [changeset_info(ch) for ch in self.content]
 
-    def read_file(self, url):
-        """Download the replicate_changeset file or read it directly from the
-        filesystem (to test purposes)."""
-        if isfile(url):
-            self.filename = url
+    def read_file(self, changeset_file):
+        """Download the replication changeset file or read it directly from the
+        filesystem (to test purposes).
+        """
+        if isfile(changeset_file):
+            self.filename = changeset_file
         else:
             self.path = mkdtemp()
-            self.filename = join(self.path, basename(url))
-            download(url, self.path)
+            self.filename = join(self.path, basename(changeset_file))
+            download(changeset_file, self.path)
 
         self.xml = ET.fromstring(gzip.open(self.filename).read())
 
     def get_area(self, geojson):
+        """Read the first feature from the geojson and return it as a Polygon
+        object.
+        """
         geojson = json.load(open(geojson, 'r'))
         self.area = Polygon(geojson['features'][0]['geometry']['coordinates'][0])
 
     def filter(self):
-        self.content = [ch for ch in self.xml.getchildren() if get_bounds(ch).intersects(self.area)]
+        """Filter the changesets """
+        self.content = [
+            ch for ch in self.xml.getchildren() if get_bounds(ch).intersects(self.area)
+        ]
 
 
 class Analyse(object):
-
+    """Analyse a changeset and define if it is suspect."""
     def __init__(self, changeset):
         self.changeset = changeset
         self.reasons = []
         self.verify_words()
 
     def verify_words(self):
+        """Verify the fields source and comment of the changeset for some
+        suspect words.
+        """
         suspect_words = [
             'google',
             'nokia',
@@ -103,6 +120,9 @@ class Analyse(object):
                     break
 
     def count(self):
+        """Count the number of elements created, modified and deleted by the
+        changeset.
+        """
         xml = get_changeset(self.changeset.get('id'))
         actions = [action.tag for action in xml.getchildren()]
         return {
