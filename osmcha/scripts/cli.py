ce0326782bea2e963c68a05cb04ad107d600829c
Wille Marcel <wille@wille.blog.br>
2017-01-31 11:18:45 -0200

    improve line coverage tests of cli

--- osmcha/scripts/cli.py
+++ osmcha/scripts/cli.py
@@ -13,8 +13,11 @@ def cli(id):
     ch.full_analysis()
     click.echo(
         'Created: %s. Modified: %s. Deleted: %s' % (ch.create, ch.modify, ch.delete)
-    )
+        )
     if ch.is_suspect:
-        click.echo('The changeset %s is suspect!' % id)
+        click.echo('The changeset {} is suspect! Reasons: {}'.format(
+            id,
+            ', '.join(ch.suspicion_reasons)
+            ))
     else:
         click.echo('The changeset %s is not suspect!' % id)
